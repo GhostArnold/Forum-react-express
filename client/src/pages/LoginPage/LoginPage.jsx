a65836b7f0e097d644bc478c18775d59e6994306
Artem <prudnikov-artyom@list.ru>
2025-05-09 15:38:26 +0300

    Сделал состояние для инпутов в авторизации

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -1,6 +1,17 @@
 import { Link } from 'react-router-dom';
 import styles from './LoginPage.module.scss';
+import { useState } from 'react';
 const LoginPage = () => {
+  // Состояния для инпутов
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    setEmail('');
+    setPassword('');
+  };
+
   return (
     <div className={styles.loginPage}>
       <article className={styles.autorization}>
@@ -12,14 +23,26 @@ const LoginPage = () => {
             </div>
           </div>
           <div className={styles.right}>
-            <form action="" className={styles.form}>
+            <form action="" className={styles.form} onSubmit={onSubmitHandler}>
               <h2>Авторизация</h2>
-              <input type="email" placeholder="Email" />
-              <input type="password" placeholder="Password" />
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
               <p>
                 <a href="">Забыли пароль?</a>
               </p>
-              <button className={styles.btn}>Войти</button>
+              <button type="submit" className={styles.btn}>
+                Войти
+              </button>
               <p className={styles.not_account}>
                 Нету акканута? <Link to="/register">Создать</Link>
               </p>
