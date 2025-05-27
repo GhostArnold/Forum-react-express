69b1d90c38e04aabb04f070ae159c84c75c64a5a
Artem <prudnikov-artyom@list.ru>
2025-05-27 13:22:58 +0300

    Сделал, чтобы мы переходили к статье по клику в разделе Популярные статьи'

--- client/src/components/PopularPost/PopularPost.jsx
+++ client/src/components/PopularPost/PopularPost.jsx
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom';
 const PopularPost = ({ post }) => {
   return (
     <div className="popular-post-item">
-      <h3>{post.title}</h3>
+      <Link to={`posts/${post._id}`}>
+        <h3>{post.title}</h3>
+      </Link>
     </div>
   );
 };
